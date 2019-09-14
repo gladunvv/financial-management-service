a84c4e31c7fc506203573a7e491c6f413c1768bf
Lancelot <bincha.1997@gmail.com>
2019-09-14 16:53:23 +0300

    Admin representation and refactoring model

--- fms_api/wallet/admin.py
+++ fms_api/wallet/admin.py
@@ -3,10 +3,24 @@ from wallet.models import Wallet, Transaction
 
 
 @admin.register(Wallet)
-class CategoryAdmin(admin.ModelAdmin):
-    pass
+class WalletAdmin(admin.ModelAdmin):
+    
+    readonly_fields = ('balance',)
+
+    list_display = ['name', 'balance']
+
+    search_fields = ['name']
+
+    ordering = ('balance',)
 
 
 @admin.register(Transaction)
-class CourseAdmin(admin.ModelAdmin):
-    pass
+class TransactionAdmin(admin.ModelAdmin):
+
+    list_display = ['type_trans', 'wallet', 'amount', 'date_time']
+
+    search_fields = ['wallet']
+
+    list_filter = ['date_time', 'type_trans', 'wallet']
+
+    ordering = ('amount', 'date_time')
--- fms_api/wallet/models.py
+++ fms_api/wallet/models.py
@@ -1,17 +1,23 @@
-from django.db import models
-from django.conf import settings
 from django.utils.translation import ugettext as _
+from django.conf import settings
+from django.db import models
+
+from datetime import datetime
 
 
 class Wallet(models.Model):
 
+    name = models.CharField(_('Wallet name'), max_length=20, unique=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name='wallets',
         on_delete=models.CASCADE
     )
-    name = models.CharField(_('Wallet name'), max_length=20, unique=True)
-    balance = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
+    balance = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        default=0.00,
+        )
 
     class Meta:
         ordering = ('balance',)
@@ -19,14 +25,18 @@ class Wallet(models.Model):
         verbose_name_plural = _('Wallets')
 
     def __str__(self):
-        return self.name
+        return self.name + ' Ballance: ' +  str(self.balance) + ' ' + 'RUB'
 
-    def can_send(self, amount):
-        return (self.balance - amount) >= 0
+    def can_send(self, amount, type_trans):
+        """Check шs a write-off operation possible"""
+        if type_trans == 'Write-off':
+            return (self.balance - amount) >= 0
+        else:
+            return True
 
-    def trans_operation(self, amount, type):
-
-        if type.CONTRIBUTION:
+    def trans_operation(self, amount, type_trans):
+        """Transfers of funds"""
+        if type_trans == 'Contribution':
             self.balance += amount
             return self.save()
         else:
@@ -36,8 +46,8 @@ class Wallet(models.Model):
 
 class Transaction(models.Model):
 
-    WRITE_OFF = 1
-    CONTRIBUTION = 2
+    WRITE_OFF = 'Write-off'
+    CONTRIBUTION = 'Contribution'
     TYPE_TRANSACTION_CHOICES = [
         (
             CONTRIBUTION,
@@ -51,12 +61,14 @@ class Transaction(models.Model):
 
     amount = models.DecimalField(_('Transaction amount'), max_digits=7, decimal_places=2)
     date_time = models.DateTimeField(_('Transaction date'), auto_now_add=True)
+    comment = models.CharField(_('User comment'), max_length=70)
     wallet = models.ForeignKey(
         Wallet,
         related_name='transactions',
         on_delete=models.CASCADE
     )
-    type = models.CharField(
+    type_trans = models.CharField(
+        _('Transaction type'),
         choices=TYPE_TRANSACTION_CHOICES,
         max_length=20
     )
@@ -64,15 +76,20 @@ class Transaction(models.Model):
     class Meta:
         verbose_name = _("Transaction")
         verbose_name_plural = _("Transactions")
-        ordering = ('date_time',)
+        ordering = ('-date_time',)
 
     def __str__(self):
-        return self.type + ' ' + str(self.amount) + ' ' + str(self.date_time)
+        return '%s %s %s %s' % (
+            self.type_trans,
+            str(self.amount),
+            'RUB',
+            self.date_time.strftime("%d.%m.%Y, %H:%M:%S")
+            )
 
     def save(self, *args, **kwargs):
         """Checking the validity of the operation"""
-        has_enough_money = self.from_wallet.can_send(self.amount)
-
+        has_enough_money = self.wallet.can_send(self.amount, self.type_trans)
+        print(has_enough_money)
         if not has_enough_money:
             return {
                 'error': _('There are not enough funds in your wallet'),
@@ -83,6 +100,6 @@ class Transaction(models.Model):
             }
         else:
             #Wallet balance update
-            self.wallet.trans_operation(self.amount, self.type)
+            self.wallet.trans_operation(self.amount, self.type_trans)
 
             return super(Transaction, self).save(*args, **kwargs)
